e46ce68086ec4e2492f6f2c3c419863f5dd0f6d7
mimibattistella <christopher.whaither.santos@escola.pr.ov.br>
2026-08-20 12:12:47 -0300

    Pagina de Calendario e Horarios

--- frontend/js/calendario.js
+++ frontend/js/calendario.js
@@ -1,31 +1,6 @@
-const dias = document.getElementById("dias");
+function selecionarDia(dia) {
 
-for(let i=1;i<=31;i++){
+    localStorage.setItem("diaSelecionado", dia);
 
-const div=document.createElement("div");
-
-div.className="dia";
-
-if(i===6 || i===13 || i===20 || i===27){
-
-div.classList.add("ocupado");
-
-}else{
-
-div.classList.add("disponivel");
-
-div.onclick=()=>{
-
-localStorage.setItem("data","2026-07-"+String(i).padStart(2,"0"));
-
-window.location="horarios.html";
-
-};
-
-}
-
-div.innerHTML=i;
-
-dias.appendChild(div);
-
+    window.location.href = "horarios.html";
 }
